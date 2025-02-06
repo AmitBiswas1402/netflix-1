d06e4aeeee047269170a5b7f4561856e045e5499
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-02-06 22:01:54 +0530

    Add movie routes for fetching trending movies and trailers

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+
 import authRoutes from "./routes/auth.routes.js";
+import movieRoutes from "./routes/movie.routes.js";
+
 import { ENV_VARS } from "./config/envVars.js";
 import { connectToDB } from "./db/db.js";
 
@@ -9,6 +12,7 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
