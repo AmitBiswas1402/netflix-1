21f2ae13e20666545784956d52bdc26870be267c
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-02-07 16:47:49 +0530

    Add TV routes and protect movie routes with authentication middleware

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,12 @@ import express from "express";
 
 import authRoutes from "./routes/auth.routes.js";
 import movieRoutes from "./routes/movie.routes.js";
+import tvRoutes from "./routes/tv.routes.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectToDB } from "./db/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+
 
 const app = express();
 const PORT = ENV_VARS.PORT;
@@ -12,7 +15,8 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/movie", protectRoute,movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -30,3 +30,42 @@ export async function getMovieTrailers(req, res) {
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 }
+
+export async function getMovieDetails(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+
+    res.json({ success: true, movie: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function getSimilarMovies(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+    );
+    res.status(200).json({ success: true, similar: data.results });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function getMoviesByCategory(req, res) {
+  const { category } = req.params;
+  try {
+    const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`)    
+    res.status(200).json({ success: true, content: data.results });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal Server Error" });    
+  }
+}
