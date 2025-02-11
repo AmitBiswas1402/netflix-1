80214191b507f5d57ac0de4cc2b63d4278698eea
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-02-11 21:20:39 +0530

    Add SearchPage and HistoryPage components; implement routing and search history functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import { useEffect } from "react";
 import { useAuthStore } from "./store/authUser";
 import { Loader } from "lucide-react";
 import WatchPage from "./pages/WatchPage.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
+import HistoryPage from "./pages/HistoryPage.jsx";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -42,6 +44,14 @@ function App() {
         <Route
           path="/watch/:id"
           element={user ? <WatchPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/search"
+          element={user ? <SearchPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/history"
+          element={user ? <HistoryPage /> : <Navigate to={"/login"} />}
         />
       </Routes>
       <Footer />
